459e593f9df40acfaf9aeaf3a2eb51727ffec376
Osmel Mendez <84472632+OsmelMdz@users.noreply.github.com>
2023-06-19 18:05:33 -0600

    Commit2

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,18 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { IsNumber, IsString } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-  @IsString()
-  nombre: string;
-  @IsString()
-  apellidos: string;
-  @IsString()
-  email: string;
-  @IsString()
-  password: string;
-  @IsString()
-  sexo: string;
-  @IsNumber()
-  edad: number;
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
@@ -8,28 +8,44 @@ import { Repository } from 'typeorm';
 @Injectable()
 export class UsersService {
   constructor(
-    @InjectRepository(User) private taskRepository: Repository<User>,
+    @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
   async create(createUserDto: CreateUserDto) {
     //return 'This action adds a new user';
-    const user = this.taskRepository.create(createUserDto);
-    await this.taskRepository.save(user);
+    const user = this.userRepository.create(createUserDto);
+    await this.userRepository.save(user);
     return user;
   }
 
   findAll() {
-    return `This action returns all users`;
+    //return `This action returns all users`;
+    const users = this.userRepository.find();
+    return users;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    //return `This action returns a #${id} user`;
+    const user = await this.userRepository.findOne({
+      where: { id },
+    });
+    if (!user) {
+      throw new BadRequestException('User no encontrado');
+    }
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    //return `This action updates a #${id} user`;
+    await this.userRepository.update(id, updateUserDto);
+    const user = this.userRepository.findOne({ where: { id } });
+    if (!user) {
+      throw new BadRequestException('No se puede actualizar');
+    }
+    return user;
   }
 
   remove(id: number) {
+    this.userRepository.delete(id);
     return `This action removes a #${id} user`;
   }
 }
